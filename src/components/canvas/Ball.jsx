98957451159b1f4689247335435b07357ecdcb8d
Heryakos Meles <heryakosmeles@gmail.com>
2024-08-21 12:11:18 +0300

    fix OrbitControls of the balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,18 +1,35 @@
-import React, { Suspense } from "react"
-import { Canvas } from "@react-three/fiber"
-import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei"
-import CanvasLoader from '../Loader'
-import { MeshStandardMaterial } from "three"
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 const Ball = (props) => {
   const [decal] = useTexture ([props.imgUrl])
   return (
-    <Float speed={1.75} rotationIntensity={1}
+    <Float speed={1.75} 
+    rotationIntensity={1}
     floatIntensity={2}>
       <ambientLight intensity={0.25}/>
       <directionalLight position={[0,0,0.05]}/>
       <mesh castShadow receiveShadow scale={2.75}>
       <icosahedronGeometry args={[1,1]}/>
-      <MeshStandardMaterial />
+      <meshStandardMaterial  
+      color='#fff8eb'
+      poLygonOffset
+      poLygonOffsetFactor={-5}
+      flatShading
+      />
+      <Decal
+      position={[0,0,1]}
+      rotation={[2 * Math.PI,0, 6.25]}
+      flatShading
+       map={decal}/>
       </mesh>
       </Float>
   )
