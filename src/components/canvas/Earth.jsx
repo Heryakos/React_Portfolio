33540ec2e671537230144d74a6431ec3379d02d7
Heryakos Meles <heryakosmeles@gmail.com>
2024-10-06 15:06:36 +0300

    fix some error while diploying

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,7 +1,7 @@
 import {Suspense} from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
-import canvasLoader from '../Loader'
+import CanvasLoader  from '../Loader'
 
 const Earth = () => {
   const earth = useGLTF('./planet/scene.gltf') 
@@ -28,7 +28,7 @@ const EarthCanvas =() => {
       position:[-4, 3,6]
     }}
     >
-<Suspense fallback ={<canvasLoader/>}>
+<Suspense fallback ={<CanvasLoader />}>
  <OrbitControls
  autoRotate
  enableZoom={false}
